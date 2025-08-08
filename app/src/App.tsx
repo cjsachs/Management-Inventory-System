e9455932280bce9b4bfefb3f3664052357c4e99c
Christian Sachs <byaskew3@gmail.com>
2025-08-07 21:10:09 -0600

    init commit

---
+++ app/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+        </a>
+        <a href="https://react.dev" target="_blank">
+        </a>
+      </div>
+      <h1>Equipment Inventory System</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
