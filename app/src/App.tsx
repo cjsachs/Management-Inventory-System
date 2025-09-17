292eff2b0b3cc8af48273eeebf7e2388d99c1fdb
cjsachs <byaskew3@gmail.com>
2025-09-17 08:32:30 -0600

    testing vercel build command

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -126,7 +126,7 @@ const App = () => {
 
       // add equipment to Firebase
       const equipmentId = await equipmentService.addEquipment(newEquipment, user.id!);
-      console.log(equipmentId)
+      console.log(equipmentId, 'test');
 
       // log activity (not implemented here, placeholder for future)
 
